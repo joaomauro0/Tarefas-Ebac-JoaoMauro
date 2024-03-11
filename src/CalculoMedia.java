db2a1affcaafd49aea165c236bc0a9ff0a241603
joao <joaom.cunha@icloud.com>
2024-03-11 13:12:23 -0300

    erro resolvido

--- src/CalculoMedia.java
+++ src/CalculoMedia.java
@@ -9,6 +9,10 @@ public class CalculoMedia {
         int num3 = 45;
         int num4= 60;
         int num5= num1 + num2 +num3 + num4;
-            System.out.println(num5/2);
+            System.out.println(num5/4);
             }
-}
+
+   }
+
+
+
