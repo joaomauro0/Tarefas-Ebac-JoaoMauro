bf903583905a100f470f0dd6610567075d34f587
joao <joaom.cunha@icloud.com>
2024-03-11 12:03:21 -0300

    exercicioCalculoMedia

---
+++ src/CalculoMedia.java
@@ -0,0 +1,14 @@
+public class CalculoMedia {
+    public static void main (String args[]){
+        mediaNotas();
+    }
+
+    public static void mediaNotas(){
+        int num1 = 20;
+        int num2= 35;
+        int num3 = 45;
+        int num4= 60;
+        int num5= num1 + num2 +num3 + num4;
+            System.out.println(num5/2);
+            }
+}
