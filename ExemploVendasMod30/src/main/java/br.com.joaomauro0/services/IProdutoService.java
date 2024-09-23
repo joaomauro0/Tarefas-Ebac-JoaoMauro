c92a21adadb717322744228d2cd71341264df662
João <118681297+joaomauro0@users.noreply.github.com>
2024-09-23 14:34:32 -0300

    Add files via upload

---
+++ ExemploVendasMod30/src/main/java/br.com.joaomauro0/services/IProdutoService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package br.com.joaomauro0.services;
+
+import br.com.rpires.domain.Produto;
+import br.com.rpires.services.generic.IGenericService;
+
+/**
+ * @author joaomauro0
+ *
+ */
+public interface IProdutoService extends IGenericService<Produto, String> {
+
+}
