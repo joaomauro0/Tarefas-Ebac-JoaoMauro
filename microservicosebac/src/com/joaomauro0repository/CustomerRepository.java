7233dd79b823709da9bc8928873a9f0554e16560
João <118681297+joaomauro0@users.noreply.github.com>
2025-02-10 17:43:11 -0300

    Delete microservicosebac directory

--- microservicosebac/src/com/joaomauro0repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.joaomauro0repository;
-
-import com.joaomauro0Model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
