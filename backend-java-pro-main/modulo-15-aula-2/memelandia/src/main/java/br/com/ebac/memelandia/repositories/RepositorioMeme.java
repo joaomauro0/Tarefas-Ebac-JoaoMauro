0b2a6c6a6cb891d3b010fe5ee4f97ea1540ca5c4
João <118681297+joaomauro0@users.noreply.github.com>
2025-02-12 17:19:52 -0300

    Delete backend-java-pro-main directory

--- backend-java-pro-main/modulo-15-aula-2/memelandia/src/main/java/br/com/ebac/memelandia/repositories/RepositorioMeme.java
+++
@@ -1,7 +0,0 @@
-package br.com.ebac.memelandia.repositories;
-
-import br.com.ebac.memelandia.entities.Meme;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositorioMeme extends JpaRepository<Meme, Long> {
-}
